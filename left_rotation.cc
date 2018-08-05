5c9f8d6764cee8ed2b2c632f362cbd61b3e93c73
Dhiraj Kumar Jain <dhirajjain0@gmail.com>
2018-08-05 16:51:10 +0530

    Left Rotation implemented in c++
    Problem Solving > DataStructures > Arrays  > LeftRotation

---
+++ left_rotation.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+int main()
+{
+	int n,d,index=0;
+	cin>>n>>d;
+	vector<int> arr (n);
+	vector<int> arr1 (n);
+	for(int i=0;i<n;i++)
+	{
+		index=i-d;
+		if(index<0)
+		index=index+n;
+		cin>>arr[index];
+	}
+	for(int j=0;j<n;j++)
+	cout<<arr[j]<<" ";
+	return 0;
+}
