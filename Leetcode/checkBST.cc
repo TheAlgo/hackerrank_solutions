434192d6d8236a26d5b84a77f6c285f42d68486d
TheAlgo <dhirajjain0@gmail.com>
2019-06-23 12:19:42 +0530

    23rd june

---
+++ Leetcode/checkBST.cc
@@ -0,0 +1,24 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+   
+bool util(TreeNode* root, TreeNode* minNode, TreeNode* maxNode) {
+    if(!root) return true;
+    if(minNode && root->val <= minNode->val || maxNode && root->val >= maxNode->val)
+        return false;
+    return util(root->left, minNode, root) && util(root->right, root, maxNode);
+}
+bool isValidBST(TreeNode* root) {
+    return util(root, NULL, NULL);
+}
+
+
+};
